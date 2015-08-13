fbda9ca981c7b12c9888c853a0199b8d9bde4192
syzygy333 <brianjonashogue@gmail.com>
2015-08-13 15:01:07 -0400

    fix year select in edit, new form

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -20,12 +20,33 @@ class ReleasesController < ApplicationController
     end
   end
 
+  def edit
+    @band = Band.find(params[:band_id])
+    @release = Release.find(params[:id])
+  end
+
+  def update
+    @band = Band.find(params[:band_id])
+    @release = Release.find(params[:id])
+    @release.band_id = @band.id
+    if current_user == nil
+      flash[:alert] = "You must be signed in to do that."
+      redirect_to band_path(@band)
+    elsif @release.update(release_params)
+      flash[:success] = "Release updated."
+      redirect_to band_path(@band)
+    else
+      flash[:alert] = @release.errors.full_messages.join(".  ")
+      render :edit
+    end
+  end
+
   private
 
   def release_params
     params.require(:release).permit(
       :band_id, :title, :track_list, :year_released, :record_label,
-      :record_label_url, :catalog_number, :wiki_link, :type, :length,
+      :record_label_url, :catalog_number, :wiki_link, :release_type, :release_length,
       :private?
     )
   end
---
+++ spec/features/user_edits_a_release_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'user edits a release', %Q{
+  As a signed up user
+  I want to edit a release
+  So that I can make the tree better
+} do
+
+  before :each do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+  end
+
+  scenario 'edits release information' do
+    band = FactoryGirl.create(:release)
+
+    visit edit_band_release_path
+    click_link "Edit Release"
+
+    fill_in "Title", with: Faker::Lorem.sentence(2)
+    fill_in "Track list", with: Faker::Lorem.sentence(2)
+    fill_in "Record label", with: Faker::Lorem.sentence(1)
+    fill_in "Record label url", with: Faker::Internet.url
+    fill_in "Catalog number", with: 50
+    fill_in "Release type", with: "CD"
+    fill_in "Release length", with: "LP"
+
+    click_button "Edit Release"
+
+    expect(page).to have_content("Release updated")
+    expect(page).to have_content(band.name)
+    expect(page).to have_content(50)
+  end
+end
