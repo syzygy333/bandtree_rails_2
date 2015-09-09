deb87998c4a5c04f13d5fe4e083ffb55e457de38
syzygy333 <brianjonashogue@gmail.com>
2015-09-09 15:06:23 -0400

    add AR relationships to tests

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,4 +1,7 @@
 describe Band do
+  it { should have_and_belong_to_many(:releases) }
+  it { should have_and_belong_to_many(:artists) }
+
   it { should have_valid(:name).when("The Mortimers") }
   it { should_not have_valid(:name).when(nil, "") }
 end
