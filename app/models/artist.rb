be8cf24eec00967aaa6ed61cbf793ac4b33cee2e
syzygy333 <brianjonashogue@gmail.com>
2015-10-14 10:10:13 -0400

    add migration, add includes to models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,10 @@
 class Artist < ActiveRecord::Base
   mount_uploader :portrait, ArtistPortraitUploader
 
+  include PgSearch
+  multisearchable :against => [:first_name, :middle_name,
+    :last_name, :stage_name]
+
   has_and_belongs_to_many :bands
   has_and_belongs_to_many :releases
 
--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,8 @@
 class Band < ActiveRecord::Base
   mount_uploader :band_photo, BandPhotoUploader
+  
+  include PgSearch
+  multisearchable :against => [:name]
 
   has_and_belongs_to_many :releases
   has_and_belongs_to_many :artists
