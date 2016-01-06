527e6f87ceac887dbfd649ca43807d50efd7cd9a
syzygy333 <brianjonashogue@gmail.com>
2016-01-06 10:17:49 -0500

    file attach/uploader test passing

--- spec/features/user_adds_a_band_spec.rb
+++ spec/features/user_adds_a_band_spec.rb
@@ -61,7 +61,6 @@ feature 'user adds a band', %Q{
     fill_in "Biography", with: band.biography
     fill_in "Official link", with: band.official_link
     fill_in "Wiki link", with: band.wiki_link
-    attach_file "Band Photo", "#{Rails.root}/spec/support/images/example_photo.jpg"
 
     click_button "Add Band"
 
@@ -69,7 +68,6 @@ feature 'user adds a band', %Q{
     expect(page).to have_content(band.name)
     expect(page).to have_content("Edit Band")
     expect(page).to have_content("Delete Band")
-    expect(band.band_photo.file_name).to eq("example_photo.jpg")
   end
 
   scenario "admin inputs invalid information" do
@@ -93,5 +91,30 @@ feature 'user adds a band', %Q{
     expect(page).to have_content("Name can't be blank")
     expect(page).to have_content("Add a band")
     expect(page).to have_content(band.biography)
+  end
+
+  scenario "admin adds band with photo" do
+    user = FactoryGirl.create(:admin)
+
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log in"
+
+    visit new_band_path
+    fill_in "Name", with: Faker::Name.name
+    fill_in "Biography", with: Faker::Lorem.paragraph(3)
+    fill_in "Official link", with: Faker::Internet.url
+    fill_in "Wiki link", with: Faker::Internet.url
+    attach_file "Band Photo", "#{Rails.root}/spec/support/images/example_photo.jpg"
+
+    click_button "Add Band"
+
+    expect(page).to have_content("Band added")
+    expect(page).to have_content("Edit Band")
+    expect(page).to have_content("Delete Band")
+    expect(Band.first.band_photo.file.filename).to eq("example_photo.jpg")
   end
 end
