e20949b972d4912b3e80de3db9a4753b8135df63
syzygy333 <brianjonashogue@gmail.com>
2016-01-19 16:24:48 -0500

    add logic for various artist and split releases

--- app/models/release.rb
+++ app/models/release.rb
@@ -12,4 +12,8 @@ class Release < ActiveRecord::Base
   validates :title, presence: true
   validates :track_list, presence: true
   validates :record_label, presence: true
+
+  def various_artists?
+    bands.length > 2 ? true : false
+  end
 end
