4ecc5ec1b4417c79f6014f4c98f098db3ddc8600
syzygy333 <brianjonashogue@gmail.com>
2016-02-02 21:22:24 -0500

    Revert "add homes controller logic and modify homepage"
    This reverts commit bb45a33cb5e51931424578a227dc87ab94bbdaeb.
    
    trying to get back to passing tests

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,4 @@
 class HomesController < ApplicationController
   def index
-    @bands = Band.all.count
-    @releases = Release.all.count
-    @artists = Artist.all.count
-    @most_recent_band = Band.last
-    @most_recent_release = Release.last
-    @most_recent_artist = Artist.last
   end
 end
